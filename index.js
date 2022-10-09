4c96eee776a50fddb78f8bb7463568149f5c4d91
Nass <nassim.ailali@edu.ece.fr>
2022-10-09 18:37:07 +0200

    Lab2

--- index.js
+++ index.js
@@ -1,21 +1,10 @@
-// Import a module
 const http = require('http')
+const handles = require('./handles.js')
+const port = 8080
 
-// Define a string constant concatenating strings
-const content = <!DOCTYPE html>
-<html> 
-    <head>
-        <meta charset="utf-8" />
-        <title>ECE AST</title>
-    </head>
-    <body>
-       <p>Hello World!</p>
-    </body>
-</html>
+http.createServer(serverHandle).listen(port, () => console.log(`Server created at localhost:$port`)) 
 
-const serverHandle = function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write(content);
-  res.end();
-}
-server.listen(localhost:8080,() => console.log("server is running at http://localhost:3003/"))
+
+
+
+ 
