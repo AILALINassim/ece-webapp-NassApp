8f514285a0331951b01f850bae63de998872e0ed
Nass <nassim.ailali@edu.ece.fr>
2022-10-11 22:36:19 +0200

    Lab3_v2

--- handles.js
+++ handles.js
@@ -1,50 +1,121 @@
 
 const fs = require('fs')
-const port = 8080
-const hostname = "127.0.0.1";
 const express = require('express')
-const app = express();
-
-app.listen(port, hostname, function () {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+const router = express.Router();
 
 
-    const content =  '<!DOCTYPE html>' +
+
+let db = {
+    articles: [
+        {
+            id: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+            title: 'My article',
+            content: 'Content of the article.',
+            date: '04/10/2022',
+            author: 'Liz Gringer'
+        },
+        // ...
+    ],
+    comments: [
+        {
+            id: '9b1deb4d-3b7d-4bad-9bdd-2b0d7b3dcb6d',
+            timestamp: 1664835049,
+            content: 'Content of the comment.',
+            articleId: '6ec0bd7f-11c0-43da-975e-2a8ad9ebae0b',
+            author: 'Bob McLaren'
+        },
+        // ...
+    ]
+}
+
+const content = '<!DOCTYPE html>' +
     '<html>' +
     '    <head>' +
     '        <meta charset="utf-8" />' +
     '        <title>ECE AST</title>' +
-    '    </head>' + 
+    '    </head>' +
     '    <body>' +
     '       <p>Hello World!</p>' +
     '    </body>' +
     '</html>'
-    
 
-    app.get("/", function (req, res) {
-        res.json(content);
-      });
-    
-    
-      app.get("/hello/:name", function (req, res) {
-        res.send(`Hello, ${req.params.name} !`);
-      });
-    
-    
-    
-      app.get("/about/:filename", function (req, res) { 
-            if (fs.existsSync("./content/"+req.params.filename)) {
-                res.writeHead(200, { 'Content-Type': 'application/json' });
-                const fileContent = require('./content/'+params.filename)
-                res.write(JSON.stringify(fileContent));
-                res.end();
-              }else {
-                res.end("File not found")
-            } 
-      });
-    
-  
+
+    router.get("/", function (req, res) {
+    res.json(content);
+});
+
+
+router.get("/hello/:name", function (req, res) {
+    res.send(`Hello, ${req.params.name} !`);
+});
+
+
+
+router.get("/about/:filename", function (req, res) {
+    if (fs.existsSync("./content/" + req.params.filename)) {
+        res.writeHead(200, { 'Content-Type': 'application/json' });
+        const fileContent = require('./content/' + req.params.filename)
+        res.write(JSON.stringify(fileContent));
+        res.end();
+    } else {
+        res.end("File not found")
+    }
+});
+
+
+router.post("/articles", (req, res) => {
+    console.log(req.body)
+
+    let article = {
+        id: req.body.id,
+        title: req.body.title,
+        content: req.body.content,
+        date: req.body.date,
+        author: req.body.author
+    }
+    db.articles.push(article)
+    res.json(article);
+    res.end()
+});
+
+
+router.get("/articles/:id", (req, res) => {
+    let article = db.articles.find(article => article.id === req.params.id)
+    if (article != null) {
+        res.json(article)
+        res.end()
+    }
+    return
+});
+
+router.get("/articles/:id/comments", (req, res) => {
+    let comments = db.comments.filter(comment => comment.articleId === req.params.id)
+    res.json(comments)
+    res.end()
+});
+
+router.post("/articles/:id/comments", (req, res) => {
+    console.log(req.body)
+    let comment = {
+        id: req.body.id,
+        timestamp: Date.now(),
+        content: req.body.content,
+        articleId: req.params.id,
+        author: req.body.author
+    }
+    db.comments.push(comment)
+    console.log(db.comments)
+    res.json(comment);
+    res.end()
+});
+
+router.get("/articles/:id/comments/:commentid", (req, res) => {
+    let comments = db.comments.filter(comment => comment.articleId === req.params.id && comment.id === req.params.commentid)
+    console.log(comments)
+    res.json(comments)
+    res.end()
+});
+
 
 /*
 const serverHandle = (req,res) => {
@@ -84,8 +155,5 @@ const serverHandle = (req,res) => {
     }
 } 
 */
-module.exports = {
-    serverHandle
- 
-}
+module.exports =router
 
