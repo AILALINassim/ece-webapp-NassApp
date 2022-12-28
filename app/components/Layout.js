a26a67a4e813cecea7d81ddd9402bd01e1c51c55
Nass <nassim.ailali@edu.ece.fr>
2022-12-28 23:50:49 +0100

    feat: darkmode and readme

--- app/components/Layout.js
+++ app/components/Layout.js
@@ -1,12 +1,23 @@
 
 import Header from './Header.js'
 import Footer from './Footer.js'
+import { useState } from 'react'
 export default function Layout({
   children
 }){
+
+  const [darkMode,setDarkMode] = useState(false);
+
+  const toggleDarkMode = ()=>{
+    setDarkMode(!darkMode);
+  }
+
   return (
-    <div>
-      <Header />
+    <div className={darkMode ? ('bg-gray-800 text-white'): ('bg-white text-black')}>
+      <Header darkMode={darkMode} />
+      <button  className="font-bold absolute" onClick={toggleDarkMode}> 
+        {darkMode ? 'Dark Mode' : 'Light Mode'}
+      </button>
       <main className="py-10 min-h-screen max-w-full md:max-w-2xl md:mx-auto">
       {children}
       </main>
